8be59c769bd8d1a7813b5b9a2d905ea32de97b71
sven30 <sven.schoovaerts@gmail.com>
2016-12-14 14:27:36 +0100

    TRM 1 commit, setted up project

---
+++ src/main/java/com/adneom/trm/Application.java
@@ -0,0 +1,21 @@
+package com.adneom.trm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
+@ComponentScan(basePackages = {"com.adneom.trm"})
+public class Application {
+	
+	public static void main (String[] args) throws Exception{
+		SpringApplication.run(Application.class);
+	}
+
+}
---
+++ src/main/java/com/adneom/trm/repository/UserRepository.java
@@ -0,0 +1,59 @@
+package com.adneom.trm.repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Repository;
+
+import com.adneom.trm.config.ConfigDBProperties;
+
+
+@Repository
+public class UserRepository {
+
+	
+	
+	public Boolean Authenticate(String Name, String Pass){
+		String stmt= "SELECT id FROM [User] "
+				+ "WHERE name = '" +Name+ "' AND password = '"+Pass+"'";
+		System.out.println(Name +" "+Pass);
+		Connection conn=null;
+		try{
+		 conn= ConfigDBProperties.openConnect();
+		
+		PreparedStatement ps= conn.prepareStatement(stmt);
+		 
+		ResultSet rs=ps.executeQuery(); 
+		
+		if(rs!=null ){
+			System.out.println("in if");
+			
+				System.out.println("before if");
+				if(rs.next()){
+					return true;
+				}
+		}
+			}
+			catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+			finally{		    
+			      try{
+			         if(conn!=null)
+			            conn.close();
+			      }catch(SQLException se){
+			         se.printStackTrace();
+			      }//end finally try
+			   }//end try
+			   System.out.println("Goodbye!");			
+		
+		return false;
+	}
+	
+	
+}
