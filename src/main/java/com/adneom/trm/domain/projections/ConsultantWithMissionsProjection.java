51eb9238827c15709f99c49765df8340c0a57986
Els De Swaef <edeswaef@adneom.com>
2017-01-25 12:13:00 +0100

    added all consultant fields to the consultant projection

--- src/main/java/com/adneom/trm/domain/projections/ConsultantWithMissionsProjection.java
+++ src/main/java/com/adneom/trm/domain/projections/ConsultantWithMissionsProjection.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.data.rest.core.config.Projection;
 
+import com.adneom.trm.domain.BusinessManager;
 import com.adneom.trm.domain.Consultant;
 import com.adneom.trm.domain.Mission;
 
@@ -13,7 +14,7 @@ import com.adneom.trm.domain.Mission;
  * The call would look as follows : 
  * 		http://[ip]:8080/consultants?projection=with_missions
  * or, for only one consultant : 
- * 		http://[ip]:8080/consultants/1?projection=with_items
+ * 		http://[ip]:8080/consultants/1?projection=with_missions
  * 
  * In order to include all the fields of the consultant, each property getter should be added in this
  * interface. It will require some extra effort in maintainability of the code. Probably there is a
@@ -23,6 +24,22 @@ import com.adneom.trm.domain.Mission;
  */
 @Projection(name = "with_missions", types=Consultant.class)
 public interface ConsultantWithMissionsProjection {
+
+	public Integer getId();
 	public String getName();
+	public String getEmail();
+	public String getInitials();
+	
+	public String getSkills();
+	public String getContact();
+	public Integer getPackag();
+	public String getHr();
+	public String getLanguages();
+	public Boolean getHasCar();
+	public Boolean getInternal();
+
+	public BusinessManager getBusinessManager();
+	public Integer getBusinessManagerId();
+	
 	public List<Mission> getMissions();
 }
