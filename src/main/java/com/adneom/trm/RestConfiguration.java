7a0d548a89dc2e08542815d607233bc54f74fac6
Els De Swaef <edeswaef@adneom.com>
2017-01-25 11:59:56 +0100

    fix + added rest documentation

--- src/main/java/com/adneom/trm/RestConfiguration.java
+++ src/main/java/com/adneom/trm/RestConfiguration.java
@@ -1,25 +1,25 @@
-package com.adneom.trm;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-
-@Configuration
-public class RestConfiguration {
-
-	@Bean
-	public CorsFilter corsFilter() {
-	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-	    CorsConfiguration config = new CorsConfiguration();
-	    config.setAllowCredentials(true); // you USUALLY want this
-	    config.addAllowedOrigin("*");
-	    config.addAllowedHeader("*");
-	    config.addAllowedMethod("GET");
-	    config.addAllowedMethod("PUT");
-	    source.registerCorsConfiguration("/**", config);
-	    return new CorsFilter(source);
-	}
-
-}
+//package com.adneom.trm;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+//import org.springframework.web.filter.CorsFilter;
+//
+//@Configuration
+//public class RestConfiguration {
+//
+//	@Bean
+//	public CorsFilter corsFilter() {
+//	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//	    CorsConfiguration config = new CorsConfiguration();
+//	    config.setAllowCredentials(true); // you USUALLY want this
+//	    config.addAllowedOrigin("*");
+//	    config.addAllowedHeader("*");
+//	    config.addAllowedMethod("GET");
+//	    config.addAllowedMethod("PUT");
+//	    source.registerCorsConfiguration("/**", config);
+//	    return new CorsFilter(source);
+//	}
+//
+//}
