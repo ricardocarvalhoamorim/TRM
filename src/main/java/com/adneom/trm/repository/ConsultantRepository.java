2a1325258bdbc1e799f4e843e866c76149ff49fb
Els De Swaef <edeswaef@adneom.com>
2017-01-25 15:47:20 +0100

    Turn of pagination for consultants for now.

--- src/main/java/com/adneom/trm/repository/ConsultantRepository.java
+++ src/main/java/com/adneom/trm/repository/ConsultantRepository.java
@@ -1,8 +1,9 @@
 package com.adneom.trm.repository;
-import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.CrudRepository;
 
 import com.adneom.trm.domain.Consultant;
 
 //@RepositoryRestResource(collectionResourceRel = "creatures", path = "creatures")
-public interface ConsultantRepository extends PagingAndSortingRepository<Consultant, Integer>/*, ConsultantCustomRepository */{
+//public interface ConsultantRepository extends PagingAndSortingRepository<Consultant, Integer>/*, ConsultantCustomRepository */{
+public interface ConsultantRepository extends CrudRepository<Consultant, Integer>/*, ConsultantCustomRepository */{
 }
