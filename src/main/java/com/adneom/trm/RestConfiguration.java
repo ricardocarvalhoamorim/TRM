73c15be8e06b67ce7026df4d7af565c249cf42c3
Els De Swaef <edeswaef@adneom.com>
2017-01-26 12:21:29 +0100

    allow delete and set very big paging size default

--- src/main/java/com/adneom/trm/RestConfiguration.java
+++ src/main/java/com/adneom/trm/RestConfiguration.java
@@ -5,9 +5,10 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class RestConfiguration {
+public class RestConfiguration extends WebMvcConfigurerAdapter {
 
 	@Bean
 	public CorsFilter corsFilter() {
@@ -19,8 +20,20 @@ public class RestConfiguration {
 	    config.addAllowedMethod("GET");
 	    config.addAllowedMethod("PUT");
 	    config.addAllowedMethod("POST");
+	    config.addAllowedMethod("DELETE");
 	    source.registerCorsConfiguration("/**", config);
 	    return new CorsFilter(source);
 	}
+
+	// Attempt for disabling pagesize everywhere... 
+//    @Override
+//    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+//        PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
+//        resolver.setOneIndexedParameters(true);
+//        resolver.setFallbackPageable(new PageRequest(1, Integer.MAX_VALUE));
+//        resolver.setMaxPageSize(Integer.MAX_VALUE);
+//        argumentResolvers.add(resolver);
+//        super.addArgumentResolvers(argumentResolvers);
+//    }
 
 }
--- src/main/java/com/adneom/trm/repository/ConsultantRepository.java
+++ src/main/java/com/adneom/trm/repository/ConsultantRepository.java
@@ -1,9 +1,8 @@
 package com.adneom.trm.repository;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
 import com.adneom.trm.domain.Consultant;
 
 //@RepositoryRestResource(collectionResourceRel = "creatures", path = "creatures")
-//public interface ConsultantRepository extends PagingAndSortingRepository<Consultant, Integer>/*, ConsultantCustomRepository */{
-public interface ConsultantRepository extends CrudRepository<Consultant, Integer>/*, ConsultantCustomRepository */{
+public interface ConsultantRepository extends PagingAndSortingRepository<Consultant, Integer>/*, ConsultantCustomRepository */{
 }
